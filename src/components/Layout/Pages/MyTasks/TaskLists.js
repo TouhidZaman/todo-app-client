b49d484f307c3d55b80c1a617a49daeaa466ce1b
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-05-19 07:02:59 +0600

    task completed handler added

--- src/components/Layout/Pages/MyTasks/TaskLists.js
+++ src/components/Layout/Pages/MyTasks/TaskLists.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import axiosInstance from '../../../../api/axiosInstance';
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import axiosInstance from "../../../../api/axiosInstance";
 import Swal from "sweetalert2";
 
-
-const TaskLists = ({tasks, setTasks}) => {
+const TaskLists = ({ tasks, setTasks }) => {
     const [loading, setLoading] = useState(false);
     //Delete Task item handler
     const handleDeleteTaskItem = async (taskId) => {
@@ -14,28 +13,43 @@ const TaskLists = ({tasks, setTasks}) => {
         if (confirm) {
             setLoading(true);
             try {
-                await axiosInstance.delete(
-                        `tasks/${taskId}`
-                    )
-                    .then((response) => {
-                        if (response.data?.acknowledged) {
-                            Swal.fire({
-                                icon: "success",
-                                title: "Your Task deleted successfully",
-                                showConfirmButton: false,
-                                timer: 1500,
-                            });
-                            const filteredItems = tasks.filter(
-                                (inventoryItem) => inventoryItem._id !== taskId
-                            );
-                            setTasks(filteredItems);
-                            setLoading(false);
-                        }
-                    });
+                await axiosInstance.delete(`tasks/${taskId}`).then((response) => {
+                    if (response.data?.acknowledged) {
+                        Swal.fire({
+                            icon: "success",
+                            title: "Your Task deleted successfully",
+                            showConfirmButton: false,
+                            timer: 1500,
+                        });
+                        const filteredItems = tasks.filter(
+                            (inventoryItem) => inventoryItem._id !== taskId
+                        );
+                        setTasks(filteredItems);
+                        setLoading(false);
+                    }
+                });
             } catch (error) {
                 console.log(error.message);
                 setLoading(false);
             }
+        }
+    };
+
+    //Task complete handler
+    const handleTaskComplete = async (taskId) => {
+        let url = `tasks/${taskId}`;
+        try {
+            await axiosInstance.put(url, { completed: true }).then((response) => {
+                if (response.data?.acknowledged) {
+                    const updatedTasks = tasks.map((task) =>
+                        task._id === taskId ? { ...task, completed: true } : task
+                    );
+                    console.log(updatedTasks);
+                    setTasks(updatedTasks);
+                }
+            });
+        } catch (error) {
+            console.log(error.message);
         }
     };
 
@@ -46,46 +60,62 @@ const TaskLists = ({tasks, setTasks}) => {
 
     return (
         <div className="overflow-x-auto">
-                <table className="table w-full">
-                    {/* <!-- head --> */}
-                    <thead>
-                        <tr>
-                            <th>Task Name</th>
-                            <th>Description</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tasks.map((task) => (
-                            <tr key={task._id}>
-                                <td>{task.task}</td>
-                                <td>{task.description}</td>
-                                <td>
+            <table className="table w-full">
+                {/* <!-- head --> */}
+                <thead>
+                    <tr>
+                        <th>Task Name</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map((task) => (
+                        <tr key={task._id}>
+                            <td>
+                                <span
+                                    className={task.completed ? "line-through" : ""}
+                                >
+                                    {task.task}
+                                </span>
+                            </td>
+                            <td>
+                                <span
+                                    className={task.completed ? "line-through" : ""}
+                                >
+                                    {task.description}
+                                </span>
+                            </td>
+                            <td>
                                 <button
-                                        // onClick={() => navigate(`/inventory/${inventoryItem._id}`)}
-                                        className="hover:text-green-300 mr-2"
-                                    >
-                                        <FontAwesomeIcon
-                                            className="text-lg pr-2"
-                                            icon={faCheckCircle}
-                                        />
-                                    </button>
-                                    <button
-                                        onClick={() => handleDeleteTaskItem(task._id)}
-                                        className="hover:text-red-500"
-                                    >
-                                        <FontAwesomeIcon
-                                            className="text-lg pr-2"
-                                            icon={faTrashAlt}
-                                        />
-                                    </button>
-                                </td>
-                                
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                                    disabled={task.completed}
+                                    onClick={() => handleTaskComplete(task._id)}
+                                    className={
+                                        task.completed
+                                            ? "line-through text-green-300 mr-2"
+                                            : "hover:text-green-300 mr-2"
+                                    }
+                                >
+                                    <FontAwesomeIcon
+                                        className="text-lg pr-2"
+                                        icon={faCheckCircle}
+                                    />
+                                </button>
+                                <button
+                                    onClick={() => handleDeleteTaskItem(task._id)}
+                                    className="hover:text-red-500"
+                                >
+                                    <FontAwesomeIcon
+                                        className="text-lg pr-2"
+                                        icon={faTrashAlt}
+                                    />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
